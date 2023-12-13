0c11acc600eb03261b676bcece796526ddf2aadf
alenico04 <94850914+alenico04@users.noreply.github.com>
2023-12-13 17:52:11 +0100

    completo

--- l02/e02/piripi/amicizia.java
+++ l02/e02/piripi/amicizia.java
@@ -1,20 +1,15 @@
 import java.util.Scanner;
-<<<<<<< Updated upstream
-public class amicizia {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        VerdeAm();
-=======
-
 public class amicizia {
     public static void main(String[] args) {
         rosso();
+        System.out.println("SIETE DIVENTATI AMICI! ora hai una persona in più a cui rompere le palle in caso di bisogno e viceversa.");
     }
 
-    static void rosso(){
+    public static void rosso(){
         Scanner sc = new Scanner(System.in);
         System.out.println("Componi il numero di telefono di una persona.");
         //int anni = sc.nextInt();
+
         System.out.println("E' in casa?");
         String risposta = sc.nextLine();
         if (risposta.equals("no")){
@@ -26,17 +21,41 @@ public class amicizia {
         if (risposta.equals("si")){
             System.out.println("Mangiate qualcosa assieme.");
         }else{
-            System.out.println("blue");
+            blue();
         }
-
         sc.close();
->>>>>>> Stashed changes
     }
 
-    public static void VerdeAm(){
+    public static void blue(){
+        System.out.println("Vuoi andare a bere qualcosa di caldo?");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("qual è la risposta? sì e no] ");
+        String risposta = sc.nextLine();
+        if (risposta.equals("Si")){
+            Scanner cc = new Scanner(System.in);
+            System.out.println("scegli tra: 1=te, 2=caffè, 3=cioccolata");
+            int bevanda = cc.nextInt();
+                if (bevanda == 1){
+                    System.out.println("Fatevi sto te");
+                }
+                else if (bevanda == 2){
+                    System.out.println("Fatevi sto caffè");
+                }
+                else if (bevanda == 3){
+                    System.out.println("Fatevi sto cioccolato");
+                }
+            cc.close();
+        }
+        else{
+            verde();
+        }
+        sc.close();
+    }
+
+    public static void verde(){
         Scanner sc = new Scanner(System.in);
         int n = 0;
-        String[] inte = new String[6];
+        String[] inte = new String[5];
         System.out.println("Allora svaghiamoci un pò...");
 
         for (int i = 0; i < 6; i++) {
@@ -44,13 +63,12 @@ public class amicizia {
             inte[n] = sc.nextLine();
             System.out.println("è una cosa che ti va di fare anche a te ?");
             String sentim = new String();
-            //do {
-            sentim = sc.nextLine();
-            //System.out.println("ciao" + n);
-            //} while (! (sentim.equals("Si")) ||  !(sentim.equals("No")));
+            do {
+                sentim = sc.nextLine();
+            } while (!((sentim.equals("Si")) ||  (sentim.equals("No"))));
             if (sentim.equals("Si")) {
                 System.out.println("Facciamo insieme questa :" + inte[n]);
-                return;
+                break;
             }else{
                 n++;
             }
@@ -60,6 +78,6 @@ public class amicizia {
         int num = sc.nextInt();
         System.out.println("Facciamo insieme questa attività :" + inte[num]);
         System.out.println("Divertitevi");
+        sc.close();
     }
-
 }
