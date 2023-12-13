5ac623e22412831998fbc9114fbc2bd2f8c4476c
Reuzo <153738618+Reuzo@users.noreply.github.com>
2023-12-13 16:29:07 +0100

    Update friendship.java

--- l02/e02/vaporeon/friendship.java
+++ l02/e02/vaporeon/friendship.java
@@ -1,15 +1,16 @@
 package l02.e02.vaporeon;
+import java.util.Scanner;
 
 public class friendship {
     public static void main(String[] args)
     {
-
+        Scanner sc = new Scanner(System.in);
 
     }
 
     static void red()
     {
-
+        
 
     }
 
