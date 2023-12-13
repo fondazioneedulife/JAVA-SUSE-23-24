d92f78306cd1c9540bbd5e188c879aae02bc47de
Francis-Scott <121269134+Francis-Scott@users.noreply.github.com>
2023-12-13 17:00:34 +0100

    Update bilaterali.java

--- l02/e02/bilaterali/bilaterali.java
+++ l02/e02/bilaterali/bilaterali.java
@@ -1,4 +1,5 @@
 package bilaterali;
+
 public class bilaterali {
     public static void main(String[] args) {
 
@@ -13,24 +14,26 @@ public class bilaterali {
         if(n == 0){
             System.out.println("allora svagiamoci un po, ");
             int i;
-            int k;
+            int k = 0;
             do{
                 System.out.println("cos'altro ti va di fare?");
                 System.out.println("mi piace? 0/no 1/si");
-                
                 int x = sc.nextInt();
+
                 if(x==0){
                     i =+ 1;
                     if(i > 6){
                         System.out.println("scegli tra tutte le opzioni quella meno disumana");
                         System.out.println("fattela piacere");
                         System.out.println("svagatevi in sieme");
+                        k = 1;
                         //siete amici
                     }
                 }else{
                     System.out.println("facciamo in sime");
                     System.out.println("svagatevi in sieme");
                     //siete amici
+                    k = 1;
                 }
 
             }while(k==0);
