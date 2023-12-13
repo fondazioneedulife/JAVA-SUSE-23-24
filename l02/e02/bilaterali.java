8eb44aa1587907e46cb1a6a7c4746aee5d986ebf
Francis-Scott <121269134+Francis-Scott@users.noreply.github.com>
2023-12-13 16:18:44 +0100

    Update bilaterali.java

---
+++ l02/e02/bilaterali.java
@@ -0,0 +1,12 @@
+
+package e02;
+public class bilaterali {
+    public static void main(String[] args) {
+        int a = function01(2, 4);
+        System.out.println(a);
+    }
+    static int function01(int b, int c){
+        int d = b + c ;
+        return d;
+    }
+}
