0854777fe4a6ae2ee188ed174e13d7a3b1c559f0
FrancescaLiboni <148268515+FrancescaLiboni@users.noreply.github.com>
2023-12-13 17:17:35 +0100

    Update friendship.java

--- l02/e02/vaporeon/friendship.java
+++ l02/e02/vaporeon/friendship.java
@@ -88,12 +88,12 @@ public class friendship {
         do{
             System.out.println("...cos'altro ti va di fare? ");
             attivita[n] = sc.nextLine();
-            System.out.println(attivita[n] + " è una cosa che va di fare anche a me? ('true' per si e 'false' per no) ");
-            boolean scelta;
+            System.out.println(attivita[n] + " è una cosa che va di fare anche a me? ('1' per si e '0' per no) ");
+            int scelta;
             do{
-                scelta = sc.nextBoolean();
-            } while (scelta || !scelta);
-            if (scelta) {
+                scelta = sc.nextInt();
+            } while (scelta == 0 || scelta == 1);
+            if (scelta == 1) {
                 System.out.println("E facciamolo assieme, dai... ");
                 rimani = false;
             } else {
