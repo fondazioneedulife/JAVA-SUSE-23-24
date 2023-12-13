a18f17ab96729534a4e3bdb89d7690574bc716db
jackycris <giacomo.cristoforicolombo@gmail.com>
2023-12-13 16:30:35 +0100

    Update torredellamorte.java

---
+++ l02/e02/torredellamorte.java
@@ -0,0 +1,15 @@
+package e02;
+
+public class torredellamorte{
+    public static void main(String[] args){
+        static int function1(){
+            
+        }
+        static int function2(){
+
+        }
+        static int function3(){
+
+        }
+    }
+}
