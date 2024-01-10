1e7c589c69ea2b3e7e9d9ceec09af3bad6b66d49
Tob-sys <cheikhtob@gmail.com>
2024-01-10 14:40:58 +0100

    Create esercizio.java

---
+++ l03/e02/Docente/esercizio.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public  class esercizio02{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Come ti chiami");
+        String nome = sc.nextline("Cheikh Tob");
+        System.out.println("Buonasera"+nome+"Cosa posso fare per te? In che anno sei nato");
+        int nascita = sc.nextInt()
+        if nascita = (< 17 )
+
+
+    
+} 
+    
+
