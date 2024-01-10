e2fd246974dabeb99db97603b92246b6d36b9601
FrancescaLiboni <148268515+FrancescaLiboni@users.noreply.github.com>
2024-01-10 14:36:52 +0100

    Update e02_francesca.java

--- l03/e02/e02_francesca.java
+++ l03/e02/e02_francesca.java
@@ -6,10 +6,13 @@ public class e02_francesca {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Come ti chiami?");
+        System.out.println("Qual è il tuo nome?");
         String nome = sc.nextLine();
-        System.out.println("Buonasera "+nome+", in che anno sei nato?");
+        System.out.println("Qual è il tuo cognome?");
+        String cognome = sc.nextLine();
+        System.out.println("Buonasera "+nome+" "+cognome+", in che anno sei nato?");
         int anno_nascita = sc.nextInt();
+        
         if (2024-anno_nascita<17){
             System.out.println("Hai meno di 17 anni, quindi ricordati che non puoi fare magie fuori da Hogwarts");
         } 
