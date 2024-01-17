735fb20358e03a5ab788a6690f973eea8200db30
Piatero <69153012+Piatero@users.noreply.github.com>
2024-01-17 17:01:44 +0100

    dd

--- l04/e05_terza_prova/pietrofornale/ex3.java
+++ l04/e05_terza_prova/pietrofornale/ex3.java
@@ -1,5 +1,48 @@
 package l04.e05_terza_prova.pietrofornale;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.lang.Math;
+
 public class ex3 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    ArrayList<Integer> arr = new ArrayList<Integer>();
+    int a;
+    
+    System.out.println("Num:");
+    int num = sc.nextInt();
+    
+    for (int i = 0; i < 6; i++) {
+      series(Math.pow(2, i));
+      System.out.println("Compare:");
+      if (sc.nextInt() == 1) {
+        num += Math.pow(2, i);
+      }
+    }
+
+    System.out.println(num);  
+  }
+
+  private static void series(double ser) {
+    ser = Math.round(ser);
+    int count = 1;
+    int check = -1;
+    int max = 62;
+
+      for (int j = 0; j < max; j++) {
+        if (check > 0) {
+          System.out.print(j + " ");
+        }
+        if (count == ser) {
+          count = 0;
+          check *= -1;
+        }
+        count++;
+      }
+    
+  }
   
+  
+
 }
