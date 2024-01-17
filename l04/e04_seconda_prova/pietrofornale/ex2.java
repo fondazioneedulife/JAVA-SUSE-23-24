1f0e01f4d918914c31a35e9f5656cbb7589f6bae
Piatero <69153012+Piatero@users.noreply.github.com>
2024-01-17 16:14:40 +0100

    Update ex2.java

---
+++ l04/e04_seconda_prova/pietrofornale/ex2.java
@@ -0,0 +1,37 @@
+//package l04.e04_seconda_prova.pietrofornale;
+
+import java.util.Scanner;
+
+public class ex2 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+  System.out.println("Un Troll nei sotterranei!\n(Io ve l’ho detto ...uh!)");
+  
+  System.out.println("Di una parola: ");
+  
+  String word = sc.nextLine();
+  int count = 0;
+
+  if (!word.equals("Vingardium Leviosa")) {
+    System.out.println("Non succede nulla, il Troll ti fissa incuriosito");
+    count++;
+  }
+
+  while (!(word.equals("Vingardium Leviosa")) && count < 3) {
+    count++;
+    System.out.println("Di una parola: ");
+    word = sc.nextLine();
+
+  }
+
+  if (word.equals("Vingardium Leviosa")){
+    System.out.println("Sei salvo");
+  
+  } else {
+    System.out.println("Splat! Il Troll ti ha spalmato sulla parete");
+  }
+  }
+
+  
+  
+}
