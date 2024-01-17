e3e88451140fd6df86b0a1ba651751fb3a66dffa
Francis-Scott <121269134+Francis-Scott@users.noreply.github.com>
2024-01-17 15:43:07 +0100

    Create francesco_b.java

---
+++ l04/e03_prima_prova/francesco_b.java
@@ -0,0 +1,10 @@
+package l04.e03_prima_prova;
+import java.util.Scanner;
+
+public class francesco_b {
+    Scanner sc = new Scanner(System.in);
+    int num = sc.nextInt();
+    switch (num) {
+        case num
+    }
+}
