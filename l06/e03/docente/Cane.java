df466d0e3e0b9fc1e13b21e7b32f375c845a0dc1
Carlo Tacchella <tacchella.carlo@gmail.com>
2024-01-29 18:58:47 +0100

    l06

---
+++ l06/e03/docente/Cane.java
@@ -0,0 +1,11 @@
+package l06.e03.docente;
+
+public class Cane{
+    private int anno;
+    public Cane(int a){
+        this.anno=a;
+    }
+    public int annoNascita(){
+        return anno;
+    }
+}
---
+++ l06/e03/docente/Negozio.java
@@ -0,0 +1,12 @@
+package l06.e03.docente;
+
+
+public class Negozio {
+    Object a;
+    public Negozio(Object b){
+        this.a = b;
+    }
+    Object adotta(){
+        return a;
+    }
+}
