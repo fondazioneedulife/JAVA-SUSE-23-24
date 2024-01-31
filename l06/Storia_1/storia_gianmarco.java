2d7f462dcd968fbf8a815900907283b255ba27a1
j <gianmarco.2004j@gmail.com>
2024-01-31 17:24:07 +0100

    Update storia_gianmarco.java

--- l06/Storia_1/storia_gianmarco.java
+++ l06/Storia_1/storia_gianmarco.java
@@ -4,8 +4,8 @@ public class storia_gianmarco {
     public static void main(String[] args) {
 
         Dettagli dettagli = new Ricordi();
-        String laMiaStoria= dettagli.getMainCharacter() +", abile "+ dettagli.getWork() + ", solcava il mare con la sua barca, la \"Sirena d'Argento\". In lontananza, spuntò una nave pirata guidata dal temibile " + dettagli.getEnemy() + ". La battaglia fu feroce, ma con astuzia, Marco respinse l'assalto.\n" + //
-                "Mentre la ciurma esultava," + dettagli.getEvent() + ". Marco e la ciurma si misero a compiere " + dettagli.getActivity() + ", dimenticando le tensioni.\n" + //
+        String laMiaStoria= dettagli.getMainCharacter()  + dettagli.getWork() + ", scavava la terra, la \"Sirena d'Argento\". In lontananza, spuntò una nave pirata guidata dal temibile " + dettagli.getEnemy() + ". La battaglia fu feroce, ma con astuzia, Mic respinse l'assalto.\n" + //
+                "Mentre la ciurma esultava," + dettagli.getEvent() + ". Mic e la ciurma si misero a compiere " + dettagli.getActivity() + ", dimenticando le tensioni.\n" + //
                 "Il sole tramontò, dipingendo il cielo di tonalità calde. Quella giornata divenne leggenda, una storia di coraggio e magia tra le onde del vasto mare.\n" + //
                 "";
         System.out.println(laMiaStoria);
