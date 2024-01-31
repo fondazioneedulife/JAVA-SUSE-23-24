8ef1f59552cce25594bbdbd1faae89f9d10ca2c3
j <gianmarco.2004j@gmail.com>
2024-01-31 16:53:48 +0100

    Update Dettagli.java

---
+++ l06/Storia_1/Dettagli.java
@@ -0,0 +1,26 @@
+package Storia_1;
+
+public class Ricordi implements Dettagli{
+
+    public String getMainCharacter() {
+        return "Paolo";
+    }
+
+    public String getEnemy() {
+        return "Pietro";
+    }
+
+    public String getWork() {
+        return "un abile marinaio";
+    }
+    
+    public String getEvent() {
+      return "arrivò una grande onda";
+    }
+
+    
+    public String getActivity() {
+       return "brindarono tutti assieme";
+    }
+    
+}
