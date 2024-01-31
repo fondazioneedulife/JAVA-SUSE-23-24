0ea493aa4756d66aecdd782a5e3d8f431dc16f20
taccarlo <tacchella.carlo@gmail.com>
2024-01-31 15:40:43 +0100

    es completo classi astratte

--- l06/e00/Docente/Soluzione.java
+++ l06/e00/Docente/Soluzione.java
@@ -1,10 +1,9 @@
 package l06.e00.Docente;
 
 public class Soluzione {
-public static void main(String[] args) {
-Cane ilMioCane = new Cane("Toby");
-System.out.println(
-    "Le prime lettere del nome sono: "
-    + ilMioCane.getNomeCaneSub());
-}
+
+    public static void main(String[] args) {
+        Fattoria f = new Fattoria();
+        f.concerto();
+    }
 }
