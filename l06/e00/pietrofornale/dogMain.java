1a4d49f7ae775aba348fbb0a966458a82a1a4036
Piatero <fornale2001@gmail.com>
2024-01-31 14:47:59 +0100

    a

---
+++ l06/e00/pietrofornale/dogMain.java
@@ -0,0 +1,11 @@
+package l06.e00.pietrofornale;
+
+public class dogMain {
+    
+    public static void main(String[] args) {
+        Dog d = new Dog("Bestia");
+
+        System.out.println(d.prefix());
+
+    }
+}
