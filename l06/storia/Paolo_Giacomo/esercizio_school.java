ca766203288f908e6defe1ddcfe9469e2598685f
GiacoFra24 <giacomo.bertoldi24@gmail.com>
2024-01-31 17:16:59 +0100

    .

---
+++ l06/storia/Paolo_Giacomo/esercizio_school.java
@@ -0,0 +1,17 @@
+package lez06;
+
+public class esercizio_school {
+    public static void main(String[] args) {
+
+        get_info d = new info_classi();
+
+        String laMiaStoria = d.getMainCharacter()+", abile "+d.getWork()+", solcava il mare con la sua barca, la \"Sirena d'Argento\". In lontananza, spuntò una nave pirata guidata dal temibile "+d.getEnemy()+". La battaglia fu feroce, ma con astuzia, Marco respinse l'assalto.\n" + //
+                "Mentre la ciurma esultava, "+d.getEvent()+". Marco e la ciurma videro"+ d.getActivity()+", e li, si risollevarono le tensioni.\n" + //
+                "Il sole tramontò, dipingendo il cielo di tonalità calde. Quella giornata divenne leggenda, una storia di coraggio e magia tra le onde del vasto mare.\n" + //
+                "";
+
+        System.out.println(laMiaStoria);
+    }
+}
+
+
---
+++ l06/storia/Paolo_Giacomo/info_classi.java
@@ -0,0 +1,27 @@
+package lez06;
+
+public class info_classi implements get_info{
+
+    public String getMainCharacter() {
+        return "El negrito";
+    }
+
+    public String getEnemy() {
+        return "Pandispagna";
+    }
+
+    public String getWork() {
+        return "furfantello";
+    }
+    
+    public String getEvent() {
+      return "arrivò l'arcangelo Gabriele";
+    }
+
+    public String getActivity() {
+       return "Maria incinta";
+    }
+    
+}
+
+
