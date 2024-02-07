fa0e11852111bb5759039486f90aa1156aaa4685
Korkerio <dedoz2003@gmail.com>
2024-02-07 15:07:39 +0100

    Create DiegoSoluzione.java

---
+++ l07/e01/DiegoE01/DiegoSoluzione.java
@@ -0,0 +1,14 @@
+package l07.e01.DiegoE01;
+
+public class DiegoSoluzione {
+        public static void main(String[] args) {
+            DiegoAreaRettangolo rettangolo = new DiegoAreaRettangolo(5, 10);
+            System.out.println("Area del rettangolo: " + rettangolo.area());
+            System.out.println(rettangolo.lati());
+    
+            DiegoParallelepipedo parallelepipedo = new DiegoParallelepipedo(5, 10, 3);
+            System.out.println("Volume del parallelepipedo: " + parallelepipedo.volume());
+            System.out.println(parallelepipedo.lati());
+        }
+    }
+
