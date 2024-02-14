846a2a10cf64f0949210d56911a378eb08d3c129
Piatero <fornale2001@gmail.com>
2024-02-14 17:29:29 +0100

    aaa

--- l08/prova01/Lannister.java
+++ l08/prova01/Lannister.java
@@ -31,7 +31,7 @@ public class Lannister {
     max = n;
     int maxi = 0;
     
-    for (int i = 1; i < 5; i++) {
+    for (int i = 1; i < 10; i++) {
       n = sc.nextInt();
       if (n > max) {
         max = n;
