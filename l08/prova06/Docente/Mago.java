8327c6e94383bd6500bcd60a57e04b641cf55034
taccarlo <tacchella.carlo@gmail.com>
2024-02-14 17:53:40 +0100

    prova 06

---
+++ l08/prova06/Docente/Mago.java
@@ -0,0 +1,18 @@
+package l08.prova06.Docente;
+
+public class Mago {
+    boolean mezzosangue;
+    boolean animagus;
+    boolean getMezzosangue(){
+        return mezzosangue;
+    }
+    boolean getAnimagus(){
+        return animagus;
+    }
+    void setMezzosangue( boolean a){
+        this.mezzosangue = a;
+    }
+    void setAnimagus(boolean b){
+        this.animagus = b;
+    }
+}
